90635b9e72655bacb5d62b5c182524546d28b159
songseogyeong <0307ssk@naver.com>
2024-05-22 16:21:59 +0900

    temp

--- teenplay_server/settings.py
+++ teenplay_server/settings.py
@@ -191,7 +191,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # 파일 접근 시
 MEDIA_URL = '/upload/'
